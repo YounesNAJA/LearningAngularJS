5b24e18225f53d3564fdbf9b849d2c552d009400
Younes NAJA <naja.younes@gmail.com>
2016-10-30 15:42:37 +0000

    $timeout service

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 var myApp = angular.module('myApp', ['ngMessages']);
-myApp.controller('mainController', ["$scope", function ($scope) {
+myApp.controller('mainController', ["$scope", "$timeout", function ($scope, $timeout) {
     $scope.name = "Younes";
+    
+    $timeout(function(){
+        $scope.name = "NAJA";
+    }, 3000);
 }]);
