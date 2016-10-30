c23330ede9f951766862696db9521e9ae8b1e8f7
Younes NAJA <naja.younes@gmail.com>
2016-10-30 17:50:43 +0000

    $filter

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 var myApp = angular.module('myApp', ['ngMessages']);
-myApp.controller('mainController', ["$scope", "$timeout", function ($scope, $timeout) {
+myApp.controller('mainController', ["$scope", "$timeout", "$filter", function ($scope, $timeout, $filter) {
     $scope.name = "Younes";
-    $scope.username = ""
+    $scope.username = "";
+    $scope.lowerusername = function() {
+        return $filter('lowercase')($scope.username);
+    };
+    
     $timeout(function(){
         $scope.name = "NAJA";
     }, 3000);
